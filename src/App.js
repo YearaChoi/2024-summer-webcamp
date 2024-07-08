ae654247029d0bfa8715b24150f33a5a627437f9
YearaChoi <22200750@handong.ac.kr>
2024-07-08 11:23:03 +0900

    feat: 경로가 변경될 때마다 페이지 상단으로 스크롤

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import MainPage from "./pages/MainPage";
 import BoardPage from "./pages/BoardPage";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<MainPage />}></Route>
         <Route path="/board/:boardTitle" element={<BoardPage />} />
